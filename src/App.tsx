14fb6c233f27f3935e2a37ba253e2364ce008815
Vitalii <vitalii@MacBook-Air-Tina.local>
2024-05-28 22:31:38 +0200

    f/002 - functionality of button done

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import React, { ChangeEvent, FormEvent } from "react";
-import "./App.css";
-import { InputTask } from "./components/listItem";
 import { useState } from "react";
 import "./App.css";
+import { InputTask } from "./components/listItem";
 import { MyButton } from "./components/myButton";
 
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<{isDone:boolean;title:string;}[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleCreateTask = () => {
+    const newTask = { title: inputValue, isDone: false };
+    setTasks((prev) => [...prev, newTask]);
+    setInputValue('');
+  };
+  const handleDoneClick = (index: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, i) =>
+        i === index ? { ...task, isDone: true } : task
+      )
+    );
+
+  }
+
 
   return (
     <>
-      <div className="bg-[#2B333B] h-[100vh] flex items-center justify-center">
-        <div className="w-[528px] min-h-[400px] flex flex-col gap-6">
+      <div className="bg-[#2B333B] h-[100vh] flex items-center justify-center text-[#E0E0E0]">
+        <div className="w-[548px] min-h-[40px] flex flex-col gap-6">
           <div>
-          <InputTask inputClass="myInputClass"/>
-          <MyButton buttonWidth={104} onClick={()=>{
-            setTasks((prev)=>[...prev, "my task"])
-          }}/>
+          <InputTask inputClass="myInputClass" value={inputValue} onChange={handleInputChange}/>
+          <MyButton buttonWidth={114} onClick={handleCreateTask}>Create</MyButton>
           </div>
           <div>
-            {tasks.map((input, index) => (
+            {tasks.map((task, index) => (
               <div>
-                <input className="mb-8 w-[400px] h-[40px] pl-2 bg-[#3A444F] border border-white rounded-lg " key={index} type="text" value={input} readOnly />
-                <MyButton buttonWidth={40} onClick={()=>{}}>Done</MyButton>
+                <input className={`${task.isDone ? "line-through" : ""} mb-8 w-[400px] h-[40px] pl-2 bg-[#3A444F] border border-white rounded-lg `} key={index} type="text" value={task.title} readOnly />
+                <MyButton buttonWidth={50} onClick={()=>{handleDoneClick(index)}}>Done</MyButton>
                 <MyButton buttonWidth={40} onClick={()=>{}}>Del</MyButton>
               </div>
               ))}
--- src/components/listItem/index.tsx
+++ src/components/listItem/index.tsx
@@ -1,14 +1,19 @@
 interface InputTaskProps {
   inputClass: string;
+  value: string;
+  status?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const InputTask: React.FC<InputTaskProps> = ({ inputClass }) => {
+export const InputTask: React.FC<InputTaskProps> = ({ inputClass, value, onChange, status}) => {
   return (
     <input
       type="text"
-      className={`w-[400px] h-[40px] ${inputClass} pl-2`}
+      className={`w-[400px] h-[40px] ${inputClass} pl-2 ${status}`}
       autoFocus
       placeholder="New Task"
+      value={value}
+      onChange={onChange}
     />
   );
 };
